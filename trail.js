604de307c7f857f6fa8b9a7074652a3392f5f7b1
KruAni <71166290+KruAni@users.noreply.github.com>
2020-10-06 00:19:14 +0530

    Add files via upload

---
+++ trail.js
@@ -0,0 +1,50 @@
+>$("h1").textContent
+"WebBrowser Class"
+
+>$("h1").innerHTML
+"Web<wbr>Browser Class"
+
+>$("meta").title
+""
+
+>$("meta").ariaDescription
+null
+
+>$("meta").ariaRoleDescription
+null
+
+>$("rank")
+null
+
+>$("meta").title
+""
+
+>$("meta").childNodes
+NodeList []
+
+>$("meta").hasChildNodes
+ƒ hasChildNodes() { [native code] }
+
+>$("meta").innerHTML
+""
+
+>$("meta").outerHTML
+"<meta charset="utf-8">"
+
+>$("meta").insertAdjacentHTML
+ƒ insertAdjacentHTML() { [native code] }
+
+>$0
+<html class=​"hasSidebar hasPageActions hasBreadcrumb reference has-default-focus js-focus-visible theme-light" lang=​"en-us" dir=​"ltr" data-css-variable-support=​"true" data-authenticated=​"false" data-auth-status-determined=​"true" data-target=​"docs" x-ms-format-detection=​"none" data-js-focus-visible>​<head>​…​</head>​<body lang=​"en-us" dir=​"ltr">​…​</body>​</html>​
+
+>var tag=document.querySelector("meta");
+undefined
+
+>tag.textContent
+""
+
+>tag.innerHTML
+""
+
+>tag
+<meta charset=​"utf-8">​
